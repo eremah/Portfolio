052b67b70f4b381cd35179f9b78512fe589cdbcf
ron <ralphyflex@gmail.com>
2023-07-16 12:50:07 +0200

    Alien invasion game

---
+++ Alien Invasion/alien _invasion.py
@@ -0,0 +1,46 @@
+import sys  # Contains tools in the sys module to exit the game when the player quits.
+import pygame  # The pygame module contains the functionality we need to make a game.
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game, create game resources"""
+        pygame.init()  # initializes the background settings that Pygame needs to work properly.
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)  # Frame rate of the game
+
+    def _check_events(self):
+        """Respond to key presses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
